2ce412597744acbe41cddebef82ab1dcf87eeaa9
LisReyes <lisset.rey2212@gmail.com>
2023-03-04 12:47:26 -0600

    Agregando la escritura en HTML

--- Ejercicio de Lógica de Programación 1/Ejercicio-1.js
+++ Ejercicio de Lógica de Programación 1/Ejercicio-1.js
@@ -1,7 +1,18 @@
 
-let a = 4;
-let b = 4;
-let c = 2;
-console.log("El valor de a es: ", a);
-console.log("El valor de b es: ", b);
-console.log("El valor de c es: ", c);
+var num1,num2,num3;
+
+num1 = Number(prompt(" Ingrese el valor del primer numero: ", " "));
+num2 = Number(prompt(" Ingrese el valor del segundo numero", " "));
+num3 = Number(prompt(" Ingrese el valor del tercer numero: ", " "));
+
+if(num1>=num2 && num2>=num3){
+    document.write("El orden ascendente de los numeros es ",num3,num2,num1);
+}else if(num2 >= num1 && num1>=num3){
+    document.write(" El orden de los numeros es ",num3,num1,num2);
+}
+if(num1>=num2 && num2>=num3){
+    document.write(" \n El orden descendente de los numero es ",num1,num2,num3);
+}else if(num2 >= num1 && num1>=num3){
+    document.write(" El orden de los numeros es ",num2,num1,num3);
+}
+
